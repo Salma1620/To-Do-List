c3802c281e52ec8ccbb5bcc9253b31fd8ed4ca02
Salma EL HASSNAOUI <elhassnaouisalma@gmail.com>
2023-10-29 19:31:40 +0100

    second commit

--- src/task.js
+++ src/task.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import circulaire from './icons/circulaire.png';
+import supprimer from './icons/supprimer.png';
+
 export default function Task(props){
     
     return(
             <li>
                 <p>{props.task}</p>
-                <div>
-                <button className="close-element" onClick={()=>props.fct(props.id)}>X</button>
-                <button className='close-element' onClick={()=>props.change(props.id)}>change</button>
+                <div className='btnsdiv'>
+                <button className='close-element' onClick={()=>props.change(props.id)}><img src={circulaire}/></button>
+                <button className="close-element" onClick={()=>props.fct(props.id)}><img src={supprimer}/></button>
                 </div>
             </li>
             
